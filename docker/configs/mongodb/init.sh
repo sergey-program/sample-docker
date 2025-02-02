c405e1ccf99c4f7a7f8c926a11a208f8b2612155
Sergey Vasilkov <sergey.program@gmail.com>
2025-02-02 06:21:33 +0300

    mongodb, not full working copy

---
+++ docker/configs/mongodb/init.sh
@@ -0,0 +1,18 @@
+set -e
+
+mongo <<EOF
+use $MONGO_INITDB_DATABASE
+
+db.createUser({
+  user: '$MONGO_USER',
+  pwd: '$MONGO_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+});
+
+db.createCollection('$MONGO_INITDB_COLLECTION');
+EOF
+
+echo "hello world!!!!!!!!!!!!"
