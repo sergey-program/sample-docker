0e0fd62556fde822292ef5041bc1da623a74ba38
Sergey Vasivlko <sergey.program@gmail.com>
2025-07-09 15:57:14 +0300

    add. new tools compose file

--- docker/scripts/down.sh
+++ docker/scripts/down.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker compose -f ./../docker-compose.yml down --rmi "local" --volumes --remove-orphans
+docker compose -f ./../docker-compose.yml -f ./../docker-compose.tools.yml down --rmi "local" --volumes --remove-orphans
